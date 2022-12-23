607d30c2dc248bdd1cc77afc56463874f898765c
mhf <804941144@qq.com>
2022-12-23 14:29:22 +0800

    feat: created new huiBo project

---
+++ src/main/java/com/mhh/huibo/HuiBoApplication.java
@@ -0,0 +1,13 @@
+package com.mhh.huibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiBoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiBoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mhh/huibo/HuiBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhh.huibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiBoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
